0743f5e5d2e07d0b2dd741a381a0b7fe658ce0d1
labjhipster <lab.jhipster@gmail.com>
2020-11-24 07:00:39 +0000

    first commit

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import './vendor';
+import { NhipsterSharedModule } from 'app/shared/shared.module';
+import { NhipsterCoreModule } from 'app/core/core.module';
+import { NhipsterAppRoutingModule } from './app-routing.module';
+import { NhipsterHomeModule } from './home/home.module';
+import { NhipsterEntityModule } from './entities/entity.module';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { MainComponent } from './layouts/main/main.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
+import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
+import { ErrorComponent } from './layouts/error/error.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    NhipsterSharedModule,
+    NhipsterCoreModule,
+    NhipsterHomeModule,
+    // jhipster-needle-angular-add-module JHipster will add new module here
+    NhipsterEntityModule,
+    NhipsterAppRoutingModule
+  ],
+  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  bootstrap: [MainComponent]
+})
+export class NhipsterAppModule {}
